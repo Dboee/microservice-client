0cf27566d731d9255eba73e66eb7f9b5f38e5eb9
Dboee <Davidboee@gmail.com>
2023-02-14 13:16:21 +0100

    implements currentUser in userProfile and Navbar

--- src/Components/UserProfile.jsx
+++ src/Components/UserProfile.jsx
@@ -7,34 +7,34 @@ import { useStateContext } from '../Contexts/ContextProvider';
 import avatar from '../Data/avatar.jpg';
 
 const UserProfile = () => {
-  const { currentColor } = useStateContext();
+  const { currentColor, currentUser } = useStateContext();
 
   return (
-    <div className="nav-item absolute right-1 top-16 bg-white dark:bg-[#42464D] p-8 rounded-lg w-96">
-      <div className="flex justify-between items-center">
-        <p className="font-semibold text-lg dark:text-gray-200">User Profile</p>
+    <div className='nav-item absolute right-1 top-16 bg-white dark:bg-[#42464D] p-8 rounded-lg w-96'>
+      <div className='flex justify-between items-center'>
+        <p className='font-semibold text-lg dark:text-gray-200'>User Profile</p>
         <Button
           icon={<MdOutlineCancel />}
-          color="rgb(153, 171, 180)"
-          bgHoverColor="light-gray"
-          size="2xl"
-          borderRadius="50%"
+          color='rgb(153, 171, 180)'
+          bgHoverColor='light-gray'
+          size='2xl'
+          borderRadius='50%'
         />
       </div>
-      <div className="flex gap-5 items-center mt-6 border-color border-b-1 pb-6">
-        <img
+      <div className='flex gap-5 items-center mt-6 border-color border-b-1 pb-6'>
+        {/* <img
           className="rounded-full h-24 w-24"
           src={avatar}
           alt="user-profile"
-        />
+        /> */}
         <div>
-          <p className="font-semibold text-xl dark:text-gray-200">
+          <p className='font-semibold text-xl dark:text-gray-200'>
             {' '}
-            Michael Roberts{' '}
+            {currentUser.username}{' '}
           </p>
-          <p className="text-gray-500 text-sm dark:text-gray-400">
+          <p className='text-gray-500 text-sm dark:text-gray-400'>
             {' '}
-            Administrator{' '}
+            {currentUser.email}{' '}
           </p>
         </div>
       </div>
@@ -42,19 +42,19 @@ const UserProfile = () => {
         {userProfileData.map((item, index) => (
           <div
             key={index}
-            className="flex gap-5 border-b-1 border-color p-4 hover:bg-light-gray cursor-pointer  dark:hover:bg-[#42464D]"
+            className='flex gap-5 border-b-1 border-color p-4 hover:bg-light-gray cursor-pointer  dark:hover:bg-[#42464D]'
           >
             <button
-              type="button"
+              type='button'
               style={{ color: item.iconColor, backgroundColor: item.iconBg }}
-              className=" text-xl rounded-lg p-3 hover:bg-light-gray"
+              className=' text-xl rounded-lg p-3 hover:bg-light-gray'
             >
               {item.icon}
             </button>
 
             <div>
-              <p className="font-semibold dark:text-gray-200 ">{item.title}</p>
-              <p className="text-gray-500 text-sm dark:text-gray-400">
+              <p className='font-semibold dark:text-gray-200 '>{item.title}</p>
+              <p className='text-gray-500 text-sm dark:text-gray-400'>
                 {' '}
                 {item.desc}{' '}
               </p>
@@ -62,13 +62,13 @@ const UserProfile = () => {
           </div>
         ))}
       </div>
-      <div className="mt-5">
+      <div className='mt-5'>
         <Button
-          color="white"
+          color='white'
           bgColor={currentColor}
-          text="Logout"
-          borderRadius="10px"
-          width="full"
+          text='Logout'
+          borderRadius='10px'
+          width='full'
         />
       </div>
     </div>
