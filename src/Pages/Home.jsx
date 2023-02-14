152e36a28a127f5fb92a2d12b118c488a99e0fab
Dboee <Davidboee@gmail.com>
2023-02-14 13:17:09 +0100

    adds contextprovider to homepage

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,10 @@ import { currentColor } from 'tailwindcss/colors';
 
 import { Loading } from '../Components/Loading';
 
+import { useStateContext } from '../Contexts/ContextProvider';
+
 export const Home = () => {
+  const {} = useStateContext();
   return (
     <div>
       <h1 className='text-3xl font-bold text-white'>Home</h1>
